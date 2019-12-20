47059f9f00260e2a0b5a3075666149b4aa42658b
adir <adir@horesh.com>
2019-12-20 18:08:41 +0200

    new

---
+++ src/main/java/com/datafilter/datafilter/DatafilterApplication.java
@@ -0,0 +1,13 @@
+package com.datafilter.datafilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatafilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatafilterApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/datafilter/datafilter/DatafilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datafilter.datafilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatafilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
