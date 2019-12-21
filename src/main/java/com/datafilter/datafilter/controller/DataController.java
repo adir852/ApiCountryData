9f59bd1a745bd5b376a8902db1da53924c4e520f
Hillelmed <bansko2013@gmail.com>
2019-12-21 18:52:32 +0200

    Add new api and controller and logic for get all data

--- src/main/java/com/datafilter/datafilter/controller/DataController.java
+++ src/main/java/com/datafilter/datafilter/controller/DataController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -23,18 +24,23 @@ public class DataController {
     private final String urlDataGov = "https://data.gov.il/api/action/datastore_search";
 
     public ResponseEntity<ArrayList<?>> getAllData() throws JsonProcessingException {
-        ResponseEntity<String> data = restTemplate.getForEntity(urlDataGov + "?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=" + totalData, String.class);
-        HashMap<String, Object> result = new ObjectMapper().readValue(data.getBody(), HashMap.class);
-        LinkedHashMap<String, Object> res = (LinkedHashMap<String, Object>) result.get("result");
-        return new ResponseEntity<ArrayList<?>>((ArrayList<?>) res.get("records"), HttpStatus.OK);
+        return getDataFromApi(null);
     }
 
-    public ResponseEntity<ArrayList<?>> getLimitData(int limit) throws JsonProcessingException {
-        ResponseEntity<String> data = restTemplate.getForEntity(urlDataGov + "?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=" + limit, String.class);
-        HashMap<String, Object> result = new ObjectMapper().readValue(data.getBody(), HashMap.class);
-        LinkedHashMap<String, Object> res = (LinkedHashMap<String, Object>) result.get("result");
-        return new ResponseEntity<ArrayList<?>>((ArrayList<?>) res.get("records"), HttpStatus.OK);
+    public ResponseEntity<ArrayList<?>> getLimitData(Integer limit) throws JsonProcessingException {
+        return getDataFromApi(limit);
     }
 
+    private ResponseEntity<ArrayList<?>> getDataFromApi(Integer limit) throws JsonProcessingException {
+        ResponseEntity<String> responseData;
+        if (limit != null && limit > 0) {
+            responseData = restTemplate.getForEntity(urlDataGov + "?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=" + limit, String.class);
+        } else {
+            responseData = restTemplate.getForEntity(urlDataGov + "?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=" + totalData, String.class);
+        }
+        HashMap<String, Object> result = new ObjectMapper().readValue(responseData.getBody(), HashMap.class);
+        HashMap<String, Object> res = (HashMap<String, Object>) result.get("result");
+        return new ResponseEntity<ArrayList<?>>((ArrayList<?>) res.get("records"), HttpStatus.OK);
+    }
 
 }
