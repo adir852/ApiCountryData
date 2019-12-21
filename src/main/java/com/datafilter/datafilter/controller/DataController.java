e26f7706c8ec424996ab156caaf16cb0bc6a8aae
Hillelmed <bansko2013@gmail.com>
2019-12-21 18:18:34 +0200

    Add new api and controller and logic for get all data

--- src/main/java/com/datafilter/datafilter/controller/DataController.java
+++ src/main/java/com/datafilter/datafilter/controller/DataController.java
@@ -20,16 +20,17 @@ public class DataController {
     RestTemplate restTemplate;
 
     private int totalData = 1272;
+    private final String urlDataGov = "https://data.gov.il/api/action/datastore_search";
 
     public ResponseEntity<ArrayList<?>> getAllData() throws JsonProcessingException {
-        ResponseEntity<String> data = restTemplate.getForEntity("https://data.gov.il/api/action/datastore_search?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=" + totalData, String.class);
+        ResponseEntity<String> data = restTemplate.getForEntity(urlDataGov + "?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=" + totalData, String.class);
         HashMap<String, Object> result = new ObjectMapper().readValue(data.getBody(), HashMap.class);
         LinkedHashMap<String, Object> res = (LinkedHashMap<String, Object>) result.get("result");
         return new ResponseEntity<ArrayList<?>>((ArrayList<?>) res.get("records"), HttpStatus.OK);
     }
 
     public ResponseEntity<ArrayList<?>> getLimitData(int limit) throws JsonProcessingException {
-        ResponseEntity<String> data = restTemplate.getForEntity("https://data.gov.il/api/action/datastore_search?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=" + limit, String.class);
+        ResponseEntity<String> data = restTemplate.getForEntity(urlDataGov + "?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=" + limit, String.class);
         HashMap<String, Object> result = new ObjectMapper().readValue(data.getBody(), HashMap.class);
         LinkedHashMap<String, Object> res = (LinkedHashMap<String, Object>) result.get("result");
         return new ResponseEntity<ArrayList<?>>((ArrayList<?>) res.get("records"), HttpStatus.OK);
